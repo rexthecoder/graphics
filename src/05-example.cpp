c7430acea503bb405f41d3c1c139774f72412135
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2023-04-10 09:03:50 +0000

    chore: added a new example

--- src/05-example.cpp
+++ src/05-example.cpp
@@ -2,53 +2,51 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <math.h>
+#include <chrono>
 
-// Frame call back that would allow us to resize our window
+
+// Resize window
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
-// Function that would allow us to process input from the keyboard
+// Process input
 void processInput(GLFWwindow *window);
 
-// Shader sources
 const GLchar *vertexSource = R"glsl(
     #version 150 core
+
     in vec2 position;
-    in vec3 color;
-    out vec3 Color;
-    void main()
-    {
-        Color = color;
+
+    void main(){
         gl_Position = vec4(position, 0.0, 1.0);
     }
 )glsl";
+
 const GLchar *fragmentSource = R"glsl(
     #version 150 core
-    in vec3 Color;
+    uniform vec3 triangleColor;
+
     out vec4 outColor;
-    void main()
-    {
-        outColor = vec4(Color, 1.0);
-    }
+
+     void main(){
+        outColor = vec4(triangleColor, 1.0);
+     }
 )glsl";
 
 int main()
 {
-    // Initialize and configure GLFW
+    auto t_start = std::chrono::high_resolution_clock::now();
+
+    // configuration
     glfwInit();
-    // Allow us to specify the heighest version of openGl we want to use
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    // Allow us to specify the lowest version of openGl we want to use
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // Allow us to specify the profile we want to use [Either Compat profile  or Core Profile]
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    // Create a window object
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Example04", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Rectangle", NULL, NULL);
 
-    // Check if the window was created successfully
-    // if not then the program is terminated
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW windows" << std::endl;
@@ -56,9 +54,8 @@ int main()
         return -1;
     }
 
-    // Make the context of the window the main context on the current thread
     glfwMakeContextCurrent(window);
-    // Set the frame call back function
+
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     //  check if all the GLAD has Load all OpenGL function pointers
@@ -68,77 +65,67 @@ int main()
         return -1;
     }
 
-    // Create Vertex Array Object
+
+
     GLuint vao;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
-    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLfloat vertices[] = {
+        0.0f, 0.5f,
+        0.5f, -0.5f,
+        -0.5f, -0.5f};
+
     GLuint vbo;
     glGenBuffers(1, &vbo);
-    GLfloat vertices[] = {
-        -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, // Top-left
-        0.5f, 0.5f, 0.0f, 1.0f, 0.0f,  // Top-right
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
-        -0.5f, -0.5f, 1.0f, 1.0f, 1.0f // Bottom-left
-    };
-
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Create an element array
-    GLuint ebo;
-    glGenBuffers(1, &ebo);
-
-    GLuint elements[] = {
-        0,
-        1,
-        2,
-        2,
-        3,
-        0,
-    };
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
-
-    // Create and compile the vertex shader
+    // Vertext shaders
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexSource, NULL);
     glCompileShader(vertexShader);
 
-    // Create and compile the fragment shader
+    // Fragment shader
+
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
     glCompileShader(fragmentShader);
 
-    // Link the vertex and fragment shader into a shader program
+    // Connecting both shaders
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
+
     glBindFragDataLocation(shaderProgram, 0, "outColor");
     glLinkProgram(shaderProgram);
     glUseProgram(shaderProgram);
 
-    // Specify the layout of the vertex data
     GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
     glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-    GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
-    glEnableVertexAttribArray(colAttrib);
-    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void *)(2 * sizeof(GLfloat)));
+    GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
+
+
+
     // Render loop
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
         // Clear the screen to black
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(0.5f, 0.6f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw a rectangle from the 2 triangles using 6 indices
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        auto t_now = std::chrono::high_resolution_clock::now();
+
+        float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+
+        glUniform3f(uniColor, (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
+
+        // Draw a rectangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwPollEvents();
         glfwSwapBuffers(window);
